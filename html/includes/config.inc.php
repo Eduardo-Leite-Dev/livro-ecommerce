2347218c5352ee338b79913fdab7798ae88d42d5
Eduardo <edusoares1992@gmail.com>
2022-04-15 07:38:13 -0300

    banco de dados e configuraçao

---
+++ html/includes/config.inc.php
@@ -0,0 +1,3 @@
+<?php
+if(!define('LIVE','')) DEFINE('LIVE', false);
+DEFINE('CONTACT_EMAIL', 'edusoares1992@gmail.com');
--- mysql.inc.php
+++ mysql.inc.php
@@ -1,7 +1,15 @@
 <?php
-DEFINE('DB_USER','username');
-DEFINE('DB_PASSWORD','password');
+DEFINE('DB_USER','root');
+DEFINE('DB_PASSWORD','');
 DEFINE('DB_HOST','localhost');
 DEFINE('DB_NAME','ecommerce');
 
 $dbc = mysqli_connect (DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+mysqli_set_charset($dbc,'utf8');
+
+function escape_data($data, $dbc){
+    
+    
+
+    return mysqli_real_escape_string($dbc,trim($data));
+}
