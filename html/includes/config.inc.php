94493de0637a3ca1cac9c198c99dc27dc0fc6719
Eduardo <edusoares1992@gmail.com>
2022-04-29 21:03:26 -0300

    arquivo de configuraçao e coneçao ao banco

--- html/includes/config.inc.php
+++ html/includes/config.inc.php
@@ -1,3 +1,31 @@
 <?php
+
+//constantes do sistema 
 if(!define('LIVE','')) DEFINE('LIVE', false);
 DEFINE('CONTACT_EMAIL', 'edusoares1992@gmail.com');
+DEFINE('LIVE', true);
+require('./includes/config.inc.php');
+DEFINE('BASE_URI','/opt/lampp/htdocs/livro-ecommerce/');
+DEFINE('BASE_URL','');
+DEFINE('MYSQL',BASE_URI.'mysql.inc.php');
+
+session_start();
+
+function my_error_handler($e_number, $e_message, $e_file, $e_line, $e_vars){
+
+    $message = "Um erro ocorreu '$e_file' e $e_line:\n$e_message\n";
+    $message .= "<pre>".print_r(debug_backtrace(), 1)."</pre>\n";
+
+    if(!LIVE){
+        echo '<div class="alert alert-danger">' .nl2br($message). '</div>';
+    }else{
+        error_log($message, 1, CONTACT_EMAIL, 'From:edusoares1992@gmail.com');
+        if($e_number != E_NOTICE){
+            echo '<div class="alert alert-danger"> ocorreu um erro de sistema. Pedimos desculpas pela inconveniência.</div>';
+        }
+    }
+
+    return true;
+}
+set_error_handler('my_error_handler');
+
--- mysql.inc.php
+++ mysql.inc.php
@@ -2,7 +2,7 @@
 DEFINE('DB_USER','root');
 DEFINE('DB_PASSWORD','');
 DEFINE('DB_HOST','localhost');
-DEFINE('DB_NAME','ecommerce');
+DEFINE('DB_NAME','ecommerce1');
 
 $dbc = mysqli_connect (DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
 mysqli_set_charset($dbc,'utf8');
